7e8b315bcbe2b9e2a170f65eaa5368ac626c8fc4
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2016-08-03 18:00:18 +0200

    Use npm package "cities" instead of internal database.

--- zip_validation.js
+++ zip_validation.js
@@ -1,50 +1,47 @@
-var express = require('express');
-var db = require('./db.js')
+'use strict';
 
 function isStateSupported(state) {
-  let states = new Set(["IL", //Illinois
-      "OR",//Oregon
-      "NV",//Nevada
-      "CO",//Colorado
-      "WA",//Washington
-      "DE",//Delaware
-      "ID",//Idaho
-      "IN",//Indiana
-      "KS",//Kansas
-      "KY",//Kentucky
-      "MA",//Massachusetts
-      "MI",//Michigan
-      "MT",//Montana
-      "NJ",//New Jersey
-      "ND",//North Dakota
-      "OH",//Ohio
-      "PA",//Pennsylvania
-      "RI",//Rhode Island
-      "SD",//South Dakota
-      "TN",//Tennessee
-      "WV",//West Virginia
-      "TX",//Texas
-      "FL",//Florida
-      "AR",//Arkansas
-      "ME",//Maine
-
-      "NH",//New Hampshire
-      "VT",//Vermont
-      "MS",//Mississippi
-      "GA"//Georgia
-    ])
-    return states.has(state)
+	let states = new Set([
+		'IL', //Illinois
+		'OR', //Oregon
+		'NV', //Nevada
+		'CO', //Colorado
+		'WA', //Washington
+		'DE', //Delaware
+		'ID', //Idaho
+		'IN', //Indiana
+		'KS', //Kansas
+		'KY', //Kentucky
+		'MA', //Massachusetts
+		'MI', //Michigan
+		'MT', //Montana
+		'NJ', //New Jersey
+		'ND', //North Dakota
+		'OH', //Ohio
+		'PA', //Pennsylvania
+		'RI', //Rhode Island
+		'SD', //South Dakota
+		'TN', //Tennessee
+		'WV', //West Virginia
+		'TX', //Texas
+		'FL', //Florida
+		'AR', //Arkansas
+		'ME', //Maine
+		'NH', //New Hampshire
+		'VT', //Vermont
+		'MS', //Mississippi
+		'GA'  //Georgia
+    ]);
+    return states.has(state);
 }
 
 function validateZIP(zip, result) {
-  db.getStateFromZIP(zip, (state) => {
-      console.log("State for ZIP:",zip, "is", state);
-      result(isStateSupported(state), state); return;
-  }, (error) => {
-    console.log(error);
-    result(false); return ;
-  })
-  if (zip==="000") {result(false); return}
+	let state =  require('cities').zip_lookup(zip).state_abbr;
+	return result(isStateSupported(state), state);
+	
+	if (zip==='000') {
+		return result(false);
+	}
 }
 
 module.exports.validateZIP = validateZIP;
